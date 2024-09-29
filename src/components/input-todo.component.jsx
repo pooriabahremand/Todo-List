1ebd73a89eb70238b837fb8bc110edac2c1e9481
pooria <bahremand@citynet.ir>
2024-09-29 14:38:48 +0330

    refactor: migrating from create-react-app to vite

---
+++ src/components/input-todo.component.jsx
@@ -0,0 +1,71 @@
+import { useContext, useRef, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import { TodoContext } from "../contexts/todo-context";
+// import { onSubmitHandler } from "../utils/utils";
+
+function TodoInput() {
+  const { setTasks, tasks } = useContext(TodoContext);
+
+  // useEffect(() => {
+  //   localStorage.setItem("regData", JSON.stringify(tasks));
+  // }, [tasks]);
+  const [inputValue, setInputValue] = useState("");
+  const [booleanCheck, setBooleanCheck] = useState(false);
+  const inputRef = useRef(null);
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    const newTask = {
+      task: inputValue,
+      isImportant: booleanCheck,
+      ID: uuidv4(),
+    };
+    let updatedTaskList;
+    if (tasks) {
+      updatedTaskList = [...tasks, newTask];
+    } else {
+      updatedTaskList = [newTask];
+    }
+    setTasks(updatedTaskList);
+    inputRef.current.focus();
+    event.target.elements[0].value = "";
+    setBooleanCheck(false);
+    localStorage.setItem("regData", JSON.stringify(updatedTaskList));
+  };
+  const inputChangeHandler = (event) => {
+    setInputValue(event.target.value);
+  };
+  const checkChangeHandler = (event) => {
+    setBooleanCheck(event.target.checked);
+  };
+  return (
+    <div className="todo_input">
+      <h1 className="toDoHeading">TO DO</h1>
+      <form onSubmit={onSubmitHandler} className="form">
+        <input
+          className="task_input"
+          type="text"
+          placeholder="What would you like to do "
+          name="todo"
+          onChange={inputChangeHandler}
+          required
+          autoFocus
+          ref={inputRef}
+        />
+        <div className="check-box">
+          <input
+            type="checkbox"
+            id="important"
+            name="important"
+            onChange={checkChangeHandler}
+            checked={booleanCheck}
+          />
+          <label htmlFor="important">important task</label>
+        </div>
+        <button type="submit">Add task</button>
+      </form>
+    </div>
+  );
+}
+
+export default TodoInput;
